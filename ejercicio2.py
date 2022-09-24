a9abb3cd895917c6a0aca3987869047abe73cfd6
Natalia Andrea Vera Torres <nataliavera2100@gmail.com>
2022-09-23 20:26:17 -0500

    Area

---
+++ ejercicio2.py
@@ -0,0 +1,7 @@
+def areaRectangulo(ancho, largo):
+    return ancho * largo 
+
+ancho = input(int("Ingrese el ancho del rectangulo: "))
+largo = input(int("Ingrese el largo del rectangulo: "))
+
+areaRectangulo(ancho, largo)
