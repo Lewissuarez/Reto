d1340972453de500b7c38e2a70ad9c7f930e2b38
209 <209@academicos.cesde.edu.co>
2022-09-23 20:31:49 -0500

    se crea funcion calcular perimetro

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,7 +1,12 @@
 def areaRectangulo(ancho, largo):
     return ancho * largo 
 
+def calcularPerimetro(ancho,largo):
+    return ancho+ancho+largo+largo
+
 ancho = input(int("Ingrese el ancho del rectangulo: "))
 largo = input(int("Ingrese el largo del rectangulo: "))
 
 areaRectangulo(ancho, largo)
+calcularPerimetro(ancho, largo)
+
